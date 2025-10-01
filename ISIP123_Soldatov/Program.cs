0f2f1c4ae0655469f6244c72b51798e347839c1f
Алексей Солдатов <alexey.soldatov.1997@gmail.com>
2025-10-01 23:22:32 +0300

    класс книга

--- ISIP123_Soldatov/Program.cs
+++ ISIP123_Soldatov/Program.cs
@@ -8,4 +8,14 @@
         ScienceFiction,
         Biography
     }
+
+    public class Book
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
 }
